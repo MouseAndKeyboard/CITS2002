2501c6b54024ed0b37dd75e50adc65c3f589c5ca
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-08-19 13:03:33 +0800

    Super secure password checker9000

--- Lab3/passwords.c
+++ Lab3/passwords.c
@@ -1,4 +1,6 @@
 
+#include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,13 +12,32 @@ int my_strlen(char *str) {
   return i;
 }
 
+bool is_safe(char *passwd) {
+  int i = 0;
+  int numbers = 0;
+  int lowercase = 0;
+  int uppercase = 0;
+  while (passwd[i] != '\0') {
+    char c = passwd[i];
+    if (isupper(c)) {
+      uppercase++;
+    } else if (islower(c)) {
+      lowercase++;
+    } else if (isdigit(c)) {
+      numbers++;
+    }
+    i++;
+  }
+  return uppercase >= 2 && lowercase >= 2 && numbers >= 2;
+}
+
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     fprintf(stderr, "Expected 1 argument, got %i\n", argc - 1);
     exit(EXIT_FAILURE);
   }
 
-  int length = my_strlen(argv[1]);
+  int length = is_safe(argv[1]);
   printf("%i\n", length);
   exit(EXIT_SUCCESS);
 }
