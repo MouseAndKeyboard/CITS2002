544e2c73d1fe9d4a6c3140225e3b985c9f9e1a94
Michael Nef <michael@nef.net.au>
2020-09-16 14:14:32 +0800

    lab 4 and money

---
+++ Lab4/money/money.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void money(int total, int *dollars, int *cents) { return; }
+
+int main(int argc, char *argv[]) {
+
+  int total = 524;
+  int dollars;
+  int cents;
+
+  money(total, &dollars, &cents);
+
+  printf("%i -> %i dollars and %i cents\n", total, dollars, cents);
+
+  return EXIT_SUCCESS;
+}
