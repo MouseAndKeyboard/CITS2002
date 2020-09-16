67ac5355d1c0831509d3d8bda47db70667fecac2
Michael Nef <michael@nef.net.au>
2020-09-16 15:19:42 +0800

    pointer based strcat

---
+++ Lab4/stringtest/pointers.c
@@ -0,0 +1,24 @@
+#include "pointers.h"
+
+char *ptr_strcat(char *dest, const char *src) {
+
+  // find the null byte on dest
+  char byte;
+  int pos = -1;
+  do {
+    pos++;
+    byte = *(dest + pos);
+  } while ('\0' != byte);
+
+  // write over the dest until we
+  // reach null byte on src
+  int src_pos = -1;
+  do {
+    src_pos++;
+    byte = *(src + src_pos);
+    *(dest + pos) = byte;
+    pos++;
+  } while ('\0' != byte);
+
+  return dest;
+}
---
+++ Lab4/stringtest/pointers.h
@@ -0,0 +1,6 @@
+#ifndef __POINTERS_H_
+#define __POINTERS_H_
+
+char *ptr_strcat(char *dest, const char *src);
+
+#endif // __POINTERS_H_
